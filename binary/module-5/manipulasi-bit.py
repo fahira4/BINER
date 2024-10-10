89e2e24bffb3e6fd265a16e44ce341cedb5335f7
Fathir-Royal <andifathir9090@gmail.com>
2024-10-10 13:22:57 +0800

    Update manipulasi_bit.py

--- binary/module-5/manipulasi-bit.py
+++ binary/module-5/manipulasi-bit.py
@@ -1,4 +1,4 @@
-def manipulasi_bit(a):
+def shift_bit(a):
 
 	# a = int(input("Masukkan bilangan bulat: "))
 
@@ -7,7 +7,7 @@ def manipulasi_bit(a):
 	# pada bagian ini, bilangan bulat akan diubah ke bilangan biner terlebih dahulu. Jika
 	# bilangannya sudah menjadi biner, maka bilangan tersebut langsung keluar
 
-	hasil_shift_left = a << 2  # Hasil: 0b101000 (40)
+	hasil_shift_left = a << 2 # Hasil: 0b101000 (40)
 	# pada bagian ini, bilangan biner tadi akan digeser sebanyak 2 kali ke kiri atau mengkalikan bulat dengan 4
 	hasil_shift_right = a >> 2 # Hasil: 0b10 (2)
 	# kebalikan dari shift kiri, yaitu membagi bilangan bulat dengan 4
@@ -16,6 +16,11 @@ def manipulasi_bit(a):
 	print(f"Shift kanan: {bin(hasil_shift_right)}") # Output: '0b10'
 	# mengeluarkan hasil pergeseran biner. bin berfungsi mengubah kembali bilangan bulat ke biner
 
+def inverse_bit(a):
+	biner_a = bin(a)[2: ]
+	print(f'Konversi ke biner: {biner_a}')
+	# pada bagian ini, bilangan bulat akan diubah ke bilangan biner terlebih dahulu. Jika
+	# bilangannya sudah menjadi biner, maka bilangan tersebut langsung keluar
 	bits = biner_a
 	# variabel baru untuk membalikkan bilangan biner
 	inverse = ''
@@ -26,15 +31,10 @@ def manipulasi_bit(a):
 
 		if i == '0':
 			inverse += '1'
-		# kalau bilangan binernya adalah 0, maka akan diubah menjadi 1 sesuai dengan codingnya 
+		# kalau bilangan binernya adalah 0, maka yang disimpan di variabel inverse adalah 1
 			
 		else:
 			inverse += '0'
-		# kalau bilangan binernya adalah 1, maka akan diubah menjadi 0. ini adalah pembalikan nilai
-		# antara 0 dan 1 saja
+		# kalau bilangan binernya adalah 1, maka yang disimpan di variabel inverse adalah 1
 			
 	print(f"Inverse-nya: {inverse}")
-
-manipulasi_bit(0b1010)
-# dalam tanda kurung itu adalah bilangan bulat. kalau ingin memasukkan bilangan biner maka harus
-# berawalan 0b. dan pada baris ke-6 cukup hapus saja kalau inputannya bilangan biner
